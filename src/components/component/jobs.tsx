e583c9c67b909a1fb9eb9db0d27bd01d8bb99f66
Faculdade Embu das Artes <contato@faculdadefaem.com.br>
2024-03-24 16:14:11 -0300

    UPDATE - Finalizando cards com as informações dos cursos

--- src/components/component/jobs.tsx
+++ src/components/component/jobs.tsx
@@ -17,28 +17,28 @@ interface JobCardProps {
 }
 
 const JobCard: React.FC<JobCardProps> = ({ imgSrc, course, type, value, sale, turn }) => (
-  <li className="flex-none md:flex  border-green-500 flex flex-col items-center w-[calc(34.3333%-1rem)] rounded-lg border border-gray-200 dark:border-white-500">
-    <div className="flex-noneflex flex-col items-center rounded-lg border border-gray-200 dark:border-white-500">
-      <img className='rounded-t-lg h-[150px] w-[250px]' src={imgSrc} alt={course} style={{ objectFit: 'cover' }} />
+  <li className="flex-none border-green-500 flex flex-col items-center w-[302px] h-[516px] rounded-lg border border-gray-200 dark:border-white-500">
+    <div className="flex flex-col items-center rounded-lg border border-gray-200 dark:border-white-500">
+      <img className='rounded-t-lg w-full' src={imgSrc} alt={course} style={{ height: '200px', objectFit: 'cover' }} />
     </div>
     <div className="mt-4 flex w-full flex-col items-center rounded-b-lg bg-medium p-4 text-white dark:bg-dark">
-              <h3 className="text-lg font-bold text-dark dark:text-white">{course}</h3>
-              <p className="text-sm text-dark dark:text-white">{type}</p>
-              <p className="text-xs line-through text-dark dark:text-white">{sale}</p>
-              <p className="text-xl font-bold text-green-500">{value}<span className="text-sm">(mensal)</span></p>
-              <p className="text-xs text-dark dark:text-white">{turn}</p>
-              <Button className="mt-2 w-full bg-green-500 text-white dark:bg-green-700 dark:text-white">Increva-se</Button>
-              <Button className="mt-2 w-full border-2 border-dark bg-medium text-dark dark:bg-dark dark:text-white dark:border-white">Saiba mais</Button>
-            </div>
-    
+      <h3 className="text-lg font-bold text-dark dark:text-white">{course}</h3>
+      <p className="text-sm text-dark dark:text-white">{type}</p>
+      <p className="text-xs line-through text-dark dark:text-white">{sale}</p>
+      <p className="text-xl font-bold text-green-500">{value}<span className="text-sm">(mensal)</span></p>
+      <p className="text-xs text-dark dark:text-white">{turn}</p>
+      <Button className="mt-2 w-full bg-green-500 text-white dark:bg-green-700 dark:text-white">Increva-se</Button>
+      <Button className="mt-2 w-full border-2 border-dark bg-medium text-dark dark:bg-dark dark:text-white dark:border-white">Saiba mais</Button>
+    </div>
   </li>
 );
 
 const Jobs: React.FC = () => {
   const carouselRef = useRef<HTMLUListElement>(null);
   // Supondo que cada card tenha 200px de largura + 16px de gap, ajuste conforme necessário.
-  const cardWidth = 200; // Largura do card
-  const gap = 16; // Espaço entre os cards
+  const cardWidth = 302; // Largura do card
+const gap = 16; // Espaço entre os cards
+const numberOfCards = 3; // Número de cards para exibir4
 
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
@@ -99,7 +99,7 @@ const onDragMove = (e: React.MouseEvent<HTMLUListElement> | React.TouchEvent<HTM
         {/* Título centralizado */}
         <h1 className="text-black text-2xl font-bold text-center my-8 dark:text-white">Conheça nossos cursos</h1>
         
-      <div className="relative max-w-6xl w-full overflow-hidden" style={{ maxWidth: `${3 * (cardWidth + gap) - gap}px` }}>
+      <div className="relative  w-full overflow-hidden" style={{ maxWidth: `${3 * (cardWidth + gap) - gap}px` }}>
         <FaAngleLeft
           className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-white rounded-full shadow-md p-3 cursor-pointer z-10"
           size="2em"
